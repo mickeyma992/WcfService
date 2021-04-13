a31d3e4070c0b11b6d507083fa9334a5547f1ba1
303828 <mimartineza@elektra.com.mx>
2021-04-13 13:32:40 -0500

    Agregar archivos de proyecto.

---
+++ WcfService1/Models/DatosPersona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WcfService1.Models
+{
+    [DataContract]
+    public class DatosPersona
+    {
+        [DataMember]
+        public string Nombre { get; set; }
+        [DataMember]
+        public int Edad { get; set; }
+        [DataMember]
+        public string Direccion { get; set; }
+
+    }
+}
---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WcfService1.Models;
+
+namespace WcfService1
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public DatosPersona GetData(int value)
+        {
+            DatosPersona objPersona = new DatosPersona();
+            objPersona.Nombre = "Marcelo";
+            objPersona.Edad = 25;
+            objPersona.Direccion ="CDMX";
+            return objPersona;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
